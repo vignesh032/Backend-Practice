5d70a967f0cac94e5a36610cc4b0290946d2fa68
vignesh032 <kyasaramvignesh03@gmail.com>
2026-06-10 21:39:17 +0530

    custom api responde and middlewares

---
+++ Day-3/Backend/src/utils/asyncHandler.js
@@ -0,0 +1,10 @@
+const asyncHandler = (fn)=>async()=>{
+    try{
+        await fn(req,res,next)
+    }catch(err){
+        res.status(err.code||500).json({
+            success:false,
+            message:err.message
+        })
+    }
+}
