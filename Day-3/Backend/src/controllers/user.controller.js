bffe6f40303d79f92508614af820d6881ebfbcf6
vignesh032 <kyasaramvignesh03@gmail.com>
2026-06-16 19:03:28 +0530

    controller config

--- Day-3/Backend/src/controllers/user.controller.js
+++ Day-3/Backend/src/controllers/user.controller.js
@@ -1,6 +1,15 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
 
 const registerUser = asyncHandler(async (req,res)=>{
+    //get user details from frontend
+    //validation - not empty
+    //check if user already exists - username,email
+    //check for images, check for avatar
+    //upload them to cloudinary, avatar
+    //create user object - create entry in db
+    //remove password and refresh token from response
+    //check user creation
+    //return res
     res.status(200).json({
         message:"ok"
     })
